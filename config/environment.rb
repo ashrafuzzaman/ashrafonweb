fa163b5d776915254aa3691e77f455f68dbf31cb
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2010-09-02 23:29:00 +0200

    Added smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+require 'smtp_tls'
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com" ,
+  :port=>587,
+  :authentication => :plain ,
+  :user_name => "noreply.ashraf@gmail.com" ,
+  :password => "imbackimback" ,
+} 
