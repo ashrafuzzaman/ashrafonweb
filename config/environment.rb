4931f79bfc67d40ebc3b4c8478d0e5b2ab48759b
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2010-09-04 02:53:23 +0200

    Improved some resume styles

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
 require 'smtp_tls'
+
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com" ,
@@ -9,4 +9,4 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain ,
   :user_name => "noreply.ashraf@gmail.com" ,
   :password => "imbackimback" ,
-} 
+}
