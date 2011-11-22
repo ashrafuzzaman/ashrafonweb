3aa258c3aa53e17de3644bf54b64345fe3579507
A.K.M. Ashrafuzzaman <jitu@Dragon.(none)>
2011-11-22 20:20:47 +0600

    Uploading Rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashrafonweb::Application.config.secret_token = '3301acf52a2e6954c75885b3ea90dede3fc3f900d1377d538d14b91ee2fa5182dd11caeafac7dabb88d3ca33c1fe84771aecc7344c03295fdf0faa5dccf55d8a'
